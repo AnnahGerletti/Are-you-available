56be90cb20b04ba67d31849c3d62baf27227f741
AnnahGerletti <agerletti@gmail.com>
2017-10-07 11:47:01 +1300

    add event admin pg includes cal compt

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import RangeCalendar from 'rc-calendar/lib/RangeCalendar'
+
+import Header from './Header'
+
+const formatStr = 'YYYY-MM-DD HH:mm:ss';
+function format(v) {
+    return v
+        ? v.format(formatStr)
+        : '';
+}
+function onStandaloneSelect(value) {
+    console.log('onSelect');
+    console.log(format(value[0]), format(value[1]));
+}
 
 export default class Register extends React.Component {
   constructor (props) {
@@ -20,13 +34,24 @@ export default class Register extends React.Component {
   }
   render () {
     return (
-      <form onSubmit={this.saveNewEvent}>
-        <p><input className="Register-form" name="name" placeholder="Event Name" type="text" onChange={(e) => this.updateNewEvent(e)}/></p>
-        <p><input className="Register-form" name="name" placeholder="Event Discription" type="textarea" onChange={(e) => this.updateNewEvent(e)}/></p>
-        <p><input className="Register-form" name="name" placeholder="Start Date" type="" onChange={(e) => this.updateNewEvent(e)}/></p>
-        <p><input className="Register-form" name="name" placeholder="End Date" type="" onChange={(e) => this.updateNewEvent(e)}/></p>
-        <input className="Register-button" type="submit"/>
-      </form>
+      <div className="App">
+        <Header name='Admin Schedule Events' />
+          <div className="Main-container">
+            <div className="Cal-container">
+                <RangeCalendar onSelect={onStandaloneSelect}/>
+                <button>Select These Dates</button>
+            </div>
+            <div className="Form-container">
+              <form onSubmit={this.saveNewEvent}>
+                <p><input className="Register-form" name="name" placeholder="Event Name" type="text" onChange={(e) => this.updateNewEvent(e)}/></p>
+                <p><input className="Register-form" name="name" placeholder="Event Discription" type="textarea" onChange={(e) => this.updateNewEvent(e)}/></p>
+                <p><input className="Register-form" name="name" placeholder="Start Date" type="" onChange={(e) => this.updateNewEvent(e)}/></p>
+                <p><input className="Register-form" name="name" placeholder="End Date" type="" onChange={(e) => this.updateNewEvent(e)}/></p>
+                <input className="Register-button" type="submit"/>
+              </form>
+            </div>
+       </div>
+    </div>
     )
   }
 }
